b0d3471a2a892a69801cc12a3303b45ec35f2782
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-03-17 19:56:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function stringChop(str, size) {
   // your code here
 	if(!str) return []
 	let chunks = []
-	for(let i = 0; i<=str.length; i=i+size){
-		let substr = str.slice(i, i+size)
-		chunk.push(substr)
+	for(let i = 0; i<str.length; i=i+size){
+		let subStr = str.slice(i, i+size)
+		chunks.push(subStr)
 	}
 	return chunks;
 }
